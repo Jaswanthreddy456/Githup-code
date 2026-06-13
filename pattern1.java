4117786f8806d915b89dfa3624adc5242b6349be
Jaswanthreddy456 <jaswanthreddyala@gmail.com>
2026-06-13 10:48:17 +0530

    Create pattern1.java

---
+++ pattern1.java
@@ -0,0 +1,15 @@
+class Main {
+    public static void main(String[] args) {
+        int n=6;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print(" ");
+            }
+            
+                System.out.print("*");
+            
+        }
+        System.out.println();
+    }
+}
+i want to print 6*6 square pattern but the only printing 1 lines of star
